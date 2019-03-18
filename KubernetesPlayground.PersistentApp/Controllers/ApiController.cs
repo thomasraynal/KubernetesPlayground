25280ae0673f6225f2a3c368e3532b5b93824fe9
Thomas Raynal <thomas.raynal2@gmail.com>
2019-03-18 17:34:58 +0100

    statefulsets

---
+++ KubernetesPlayground.PersistentApp/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KubernetesPlayground.PersistentApp.Controllers
+{
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok("This is the api root. K8s ingress mount the api on /api and check this for service health.");
+        }
+    }
+}
---
+++ KubernetesPlayground.PersistentApp/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace KubernetesPlayground.PersistentApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private IHttpContextAccessor _context;
+        private string _folder = "files";
+
+        public FilesController(IHttpContextAccessor context)
+        {
+            Directory.CreateDirectory(_folder);
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Index()
+        {
+            return Ok(Directory.GetFiles(_folder).Select(path => Path.GetFileName(path)));
+        }
+
+        [HttpPut("{name}")]
+        public ActionResult Put(string name, [FromBody] dynamic value)
+        {
+            System.IO.File.AppendAllText(Path.Combine(_folder, name), (value as JObject).ToString());
+
+            return Ok(new { host = _context.HttpContext.Request.Host.Host, file = name });
+        }
+
+    }
+}
